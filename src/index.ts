74a00e7dccd798f0fc65e20499eb4bcf6ecb3312
Lay <karomahlailatul@gmail.com>
2024-03-23 15:48:52 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,6 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
